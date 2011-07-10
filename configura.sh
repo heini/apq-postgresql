27b50f3142f34c58e21707f73bdc26f1f1216e40
Daniel Norte de Moraes <danielcheagle@gmail.com>
2011-07-10 20:55:02 -0300

    new build system. backup of atual work take 12. next to a working configura.sh take 2  :-)

--- configura.sh
+++ configura.sh
@@ -12,150 +12,155 @@
 
 
 if [ $# -ne 9 ]; then
+	printf ' You dont need use it by hand. read INSTALL for more info and direction.  \n'
 	printf 'configura "OSes" "libtypes,libtypes_n" "paths_for_compiler:paths_for_compiler_n" "system_libs_paths:system_libs_paths_n"  "ssl_include_paths" "path_pg_config"  "path_gprconfig"  "path_gprbuild"  "with_debug_too"  \n'
 	exit 1
 fi;
 
-my_oses=""
-   _oses="$1"
-my_libtypes=""
-_libtypes="$2"
+ifsbackup="$IFS"
+
+
 my_version=$(cat version)
 my_atual_dir=$(pwd)
-_base_name=""
-my_paths_for_compiler="$3"
 
-my_system_libs_paths=""
-_system_libs_paths="$4"
+IFS=
 
-my_ssl_include_paths=""
-_ssl_include_paths="$5"
-my_pg_config_path=""
-_pg_config_path="$6"
-my_gprconfig_path=""
-_gprconfig_path="$7"
-my_gprbuild_path=""
-_gprbuild_path="$8"
+my_oses=
+_oses=$1
+my_libtypes=
+_libtypes=$2
+_base_name=
+my_paths_for_compiler=$3
 
-my_with_debug_too=""
-_with_debug_too="$9"
+my_system_libs_paths=
+_system_libs_paths=$4
 
-ifsbackup="$IFS"
+my_ssl_include_paths=
+_ssl_include_paths=$5
+my_pg_config_path=
+_pg_config_path=$6
+my_gprconfig_path=
+_gprconfig_path=$7
+my_gprbuild_path=
+_gprbuild_path=$8
+
+my_with_debug_too=
+_with_debug_too=$9
 
 # fix me if necessary:
 # need more sanatization
 
-my_system_libs_paths="$_system_libs_paths"
-my_ssl_include_paths="$_ssl_include_paths"
-my_pg_config_path="$_pg_config_path"
-my_gprconfig_path="$_gprconfig_path"
-my_gprbuild_path="$_gprbuild_path"
-
-_oses=${_oses:-"linux"}${_oses}
-
-_oses=${_oses,,}	# to_lower. min bash v4.0
-
-for r in linux mswindows darwin bsd other
-do
-	case "$_oses" in
-		*all*) my_oses="linux,mswindows,darwin,bsd,other";
-			break ;;
-		*"$r"*) my_oses=${my_oses:+"${my_oses},"}${r} ;;
-	esac
-done
-
-_libtypes=${_libtypes:-"dynamic,static"}${_libtypes}
-_libtypes=${_libtypes,,}   # to_lower
-
-unset r
-for r in static dynamic relocatable
-do
-	case "$_libtypes" in
-		*all*)	my_libtypes="static,dynamic,relocatable" ;
-			break ;;
-		*"$r"*)	my_libtypes=${my_libtypes:+"${my_libtypes},"}${r} ;;
-	esac
-done
-
-my_libtypes=${my_libtypes:-"static,dynamic"}${my_libtypes}
-
-my_system_libs_paths=${_system_libs_paths:-"/usr/lib"}${_system_libs_paths}
-
-my_ssl_include_paths=${_ssl_include_paths:-"/usr/lib/openssl"}${_ssl_include_paths}
-
-my_with_debug_too=${_with_debug_too,,}	#to_lower
-
-case $my_with_debug_too in
-	*yes*)	my_with_debug_too="normal,debug" ;;
-	*no*)	my_with_debug_too="normal" ;;
-esac
-
-my_with_debug_too=${my_with_debug_too:-"normal"}${my_with_debug_too}
-
-libdir2=""
-
-IFS=";:"
-for alibdirsystem in $my_system_libs_paths
-do
-	libdir2="${libdir2:+${libdir2},}\" L${alibdirsystem}\" "
-done
-
-my_system_libs_paths="${libdir2}"
-libdir2="( ${libdir2}  )"	#note: min of two spaces before ")"  :-)
+my_system_libs_paths=$_system_libs_paths
+my_ssl_include_paths=$_ssl_include_paths
+my_pg_config_path=$_pg_config_path
+my_gprconfig_path=$_gprconfig_path
+my_gprbuild_path=$_gprbuild_path
 
 IFS="$ifsbackup"
 
-made_dirs="${my_atual_dir}/build/"
+_oses=${_oses:-linux}${_oses}
+_oses=${_oses,,}
+# ${var,,) -> to_lower(var). min bash v4.0
 
-IFS=","
+for r in linux mswindows darwin bsd other
+do
+	case $_oses in
+		*all*) my_oses=linux,mswindows,darwin,bsd,other
+			break
+			;;
+		*"$r"*) my_oses=${my_oses:+${my_oses},}$r
+			;;
+	esac
+done
+
+_libtypes=${_libtypes,,}
+# to_lower
+
+for r2 in static dynamic relocatable
+do
+	case $_libtypes in
+		*all*)	my_libtypes=static,dynamic,relocatable
+			break
+			;;
+		*"$r2"*)	my_libtypes=${my_libtypes:+${my_libtypes},}$r2
+			;;
+	esac
+done
+
+my_libtypes=${my_libtypes:-static,dynamic}$my_libtypes
+
+my_system_libs_paths=${_system_libs_paths:-/usr/lib}$_system_libs_paths
+
+my_ssl_include_paths=${_ssl_include_paths:-/usr/lib/openssl}$_ssl_include_paths
+
+_with_debug_too=${_with_debug_too,,}
+#to_lower
+
+case $_with_debug_too in
+	*yes*)	my_with_debug_too=normal,debug
+		;;
+	*no*)	my_with_debug_too=normal
+		;;
+esac
+
+my_with_debug_too=${my_with_debug_too:-normal}$my_with_debug_too
+
+libdir2=
+libdir3=
+
+IFS=";:$ifsbackup"
+for alibdirsystem in $my_system_libs_paths
+do
+	libdir2="${libdir2:+${libdir2},}\" L${alibdirsystem}\" "
+	libdir3="${libdir3:+$libdir3} L${alibdirsystem} "
+done
+
+my_system_libs_paths="$libdir3"
+libdir2="( ${libdir2}  )"
+#note: min of two spaces before ")"  :-)
+
+IFS=",$ifsbackup"
+
+made_dirs=${my_atual_dir}/build
+
 
 for sist_oses in $my_oses
 do
-
-	IFS=","
 	for libbuildtype in $my_libtypes
 	do
-		# normal,debug
-		IFS=","
 		for debuga in $my_with_debug_too
 		do
-			IFS="$ifsbackup"
-
-
-			my_tmp="{$made_dirs}/${sist_oses}/${libbuildtype}/${debuga}"
+			my_tmp="$made_dirs"/$sist_oses/$libbuildtype/$debuga
 			mkdir -p "$my_tmp"
+
 				#min two spaces before "\n" because quotes
-			{	printf	"${my_tmp}  \n"
-				printf	"${debuga}  \n"
-				printf	"${libbuildtype}  \n"
-				printf	"${sist_oses}  \n"
-				printf	"${my_paths_for_compiler}  \n"
-				printf	"${my_gprconfig_path}  \n"
-				printf	"${my_gprbuild_path}  \n"
-				printf	"${my_pg_config_path}  \n"
-			}>"${my_tmp}/kov.log"
+			{	printf	"$my_tmp  \n"
+				printf	"$debuga  \n"
+				printf	"$libbuildtype  \n"
+				printf	"$sist_oses  \n"
+				printf	"$my_paths_for_compiler  \n"
+				printf	"$my_gprconfig_path  \n"
+				printf	"$my_gprbuild_path  \n"
+				printf	"$my_pg_config_path  \n"
+			}>"$my_tmp/kov.log"
+
 				#min two spaces before "\n" because quotes
-			{	printf	"version:=\"${my_version}\"  \n"
-				printf	"system_libs:=${libdir2}  \n"	
-				printf	"myhelpsource:=\"${my_atual_dir}/src-c/\"  \n"
-				printf	"mysource:=\"${my_atual_dir}/src/\"  \n"
-				printf	"basedir:=\"${my_atual_dir}/build\"  \n"
-			}>"${my_tmp}/kov.def"
+			{	printf	"version:=\"$my_version\"  \n"
+				printf	"system_libs:=$libdir2  \n"	
+				printf	"myhelpsource:=\"$my_atual_dir/src-c/\"  \n"
+				printf	"mysource:=\"$my_atual_dir/src/\"  \n"
+				printf	"basedir:=\"$my_atual_dir/build\"  \n"
+			}>"$my_tmp/kov.def"
 
-			gnatprep "${my_atual_dir}/apq_postgresql_version.gpr.in"  "${my_tmp}/apq_postgresql_version.gpr"  "${my_tmp}/kov.def"
-			cp "${my_atual_dir}/apq-postgresql.gpr"  "${my_tmp}/"
-			cp "${my_atual_dir}/apq_postgresqlhelp.gpr"  "${my_tmp}/"
+			gnatprep "$my_atual_dir/apq_postgresql_version.gpr.in"  "$my_tmp/apq_postgresql_version.gpr"  "$my_tmp/kov.def"
+			cp "$my_atual_dir/apq-postgresql.gpr"  "$my_tmp/"
+			cp "$my_atual_dir/apq_postgresqlhelp.gpr"  "$my_tmp/"
 
-			IFS=","
-
-			for support_dirs in obj,lib_ali,ali,obj_c,lib_ali_c,ali_c
+			for support_dirs in obj lib_ali ali,obj_c lib_ali_c ali_c
 			do
-				IFS="$ifsbackup"
-
-				mkdir -p "{$made_dirs}/${sist_oses}/${libbuildtype}/${debuga}/${support_dirs}"
-				
-				IFS=","
-
+			
+				mkdir -p "$my_tmp"/$support_dirs
+			
 
 			done # support_dirs
 		done # debuga
